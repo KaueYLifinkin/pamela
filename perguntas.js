eed5b9c1b8d7ffa767f66ecfc5e7ad7d89408642
KaueYLifinkin <152009280+KaueYLifinkin@users.noreply.github.com>
2024-11-07 10:58:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'culinária asiática',
+    'que ingredientes leva o yakissoba?',
+    'macarrão, carne, frango, brocolis, cenoura e temperos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'animais',
+    'quantos anos vime um gato?',
+    'de 13 a 20 anos'
+)
+
+criaCartao(
+    'idiomas',
+    'Como se diz bom dia em espanhol?',
+    'bom dia em espanhol se diz buenos dias'
+)
